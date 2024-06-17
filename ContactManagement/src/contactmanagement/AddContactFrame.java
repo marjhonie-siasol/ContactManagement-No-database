1d1f30d271e03b4e43521395c2dae0127ad9f678
Marjhonie <157345383+marjhonie-siasol@users.noreply.github.com>
2024-06-18 05:00:21 +0800

    Update AddContactFrame.java

--- ContactManagement/src/contactmanagement/AddContactFrame.java
+++ ContactManagement/src/contactmanagement/AddContactFrame.java
@@ -15,7 +15,8 @@ public class AddContactFrame extends javax.swing.JFrame {
                 this.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
                 this.model = model;     
         }
-      
+
+        
         @SuppressWarnings("unchecked")
         // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
         private void initComponents() {
